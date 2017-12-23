87e8228527c20ed6c37055946fd8ce3466480abe
FulcrumVlsM <misha867458@gmail.com>
2017-12-24 00:26:27 +0600

    Контроллер Person закончен. Не тестировал.

--- ElJournal/Controllers/PersonsController.cs
+++ ElJournal/Controllers/PersonsController.cs
@@ -104,6 +104,7 @@ namespace ElJournal.Controllers
         }
 
         // POST: api/Persons
+        //Authorization: personId
         public async Task<dynamic> Post([FromBody]Person person)
         {
             Response response = new Response(); //формат ответа
@@ -146,11 +147,55 @@ namespace ElJournal.Controllers
         }
 
         // PUT: api/Persons/5
-        public async Task<dynamic> Put(int id, [FromBody]Person person)
+        //Authorization: personId
+        public async Task<dynamic> Put(string id, [FromBody]Person person)
         {
             Response response = new Response(); //формат ответа
             string authorId = Request.Headers.Authorization?.Scheme; //id пользователя из заголовка http
             Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string sqlQuery = "dbo.UpdatePerson";
+
+            try
+            {
+                DB db = DB.GetInstance();
+                bool right = await db.CheckPermission(authorId, PERSON_CRUD_PERMISSION);
+                if (right)//если у пользователя есть права на операцию
+                {
+                    parameters.Add("@ID", id);
+                    parameters.Add("@RolesID", person.RolesId);
+                    parameters.Add("@name", person.name);
+                    parameters.Add("@student_id", person.student_id);
+                    parameters.Add("@passport_id", person.passport_id);
+                    parameters.Add("@avn_login", person.avn_login);
+                    parameters.Add("@info", person.info);
+                    int res = db.ExecStoredProcedure(sqlQuery, parameters);
+                    if (res == 0)
+                    {
+                        response.Succesful = true;
+                        response.message = "Person data was changed";
+                    }
+                    else
+                        response.message = "Person data was't changed";
+                }
+                else
+                    response.Error = ErrorMessage.PERMISSION_ERROR;
+            }
+            catch(Exception e)
+            {
+                response.Error = e.ToString();
+                response.message = e.Message;
+            }
+            return response;
+        }
+
+        // DELETE: api/Persons/5
+        //Authorization: personId
+        public async Task<dynamic> Delete(string id)
+        {
+            Response response = new Response(); //формат ответа
+            string authorId = Request.Headers.Authorization?.Scheme; //id пользователя из заголовка http
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string sqlQuery = "delete from Persons where ID=@id";
 
             try
             {
@@ -158,15 +203,26 @@ namespace ElJournal.Controllers
                 bool right = await db.CheckPermission(authorId, PERSON_CRUD_PERMISSION);
                 if (right)
                 {
-
+                    parameters.Add("@id", id);
+                    int res = db.ExecInsOrDelQuery(sqlQuery, parameters);
+                    if (res != 0)  //TODO: возможно лучше сделать условие res==1, чтобы гарантировать удаление одной записи
+                    {
+                        response.Succesful = true;
+                        response.message = "Department was delete";
+                    }
+                    else
+                        response.message = "Department wasn't delete";
                 }
+                else
+                    response.Error = ErrorMessage.PERMISSION_ERROR;
             }
-        }
+            catch(Exception e)
+            {
+                response.Error = e.ToString();
+                response.message = e.Message;
+            }
 
-        // DELETE: api/Persons/5
-        public async Task<dynamic> Delete(int id)
-        {
-            return null;
+            return response;
         }
 
 
