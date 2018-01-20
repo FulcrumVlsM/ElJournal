a1d23ecb87f5da75292fb5f9e36659450eed28b5
FulcrumVlsM <misha867458@gmail.com>
2018-01-20 15:13:49 +0600

    Контроллер LabWork - определение методов и маршрутов

--- ElJournal/Controllers/LabWorkController.cs
+++ ElJournal/Controllers/LabWorkController.cs
@@ -3,37 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using ElJournal.Models;
 
 namespace ElJournal.Controllers
 {
     public class LabWorkController : ApiController
     {
+        /* Лабораторные работы связываются с предметом (SubjectGroupSemester).
+         * Для связи лабораторной работы с преметом используются методы с роутингом /plan.
+         * Студент связывается с лаб-предмет.
+         * Для установки отметки сдал/не_сдал используются методы с роутингом /execute.
+         */
+        
+
         // GET: api/LabWork
-        public IEnumerable<string> Get()
+        //TODO: возвращает полный список лабораторных работ (без самих файлов)
+        public async Task<dynamic> Get([FromUri]string name)
         {
-            return new string[] { "value1", "value2" };
+            return null;
         }
+
+
+        //возаращает список лабораторных по указанному предмету
+        [HttpGet]
+        [Route("api/LabWork/plan/{id}")]
+        public async Task<dynamic> GetPlan(string id)
+        {
+            return null;
+        }
+
+
+        //возвращает список лабораторных, выполненных студентом по предмету
+        [HttpGet]
+        [Route("api/LabWork/exec/{studentId}/{subjectId}")]
+        public async Task<dynamic> GetExec(string studentId, string subjectId)
+        {
+            return null;
+        }
+
 
         // GET: api/LabWork/5
-        public string Get(int id)
+        // возвращает конкретную лабораторную работу (параметр file указывает нужно ли содержимое файла)
+        public async Task<dynamic> Get(string id, [FromUri]bool file=false)
         {
-            return "value";
+            return null;
         }
+
+
+        // POST: api/LabWork/plan/5/5
+        // добавление лабораторной работы в план по предмету
+        [HttpPost]
+        [Route("api/LabWork/plan/{subjectSemesterId}/{workId}")]
+        public async Task<dynamic> PostPlan(string subjectSemesterId, string workId)
+        {
+            return null;
+        }
+
+
+        // POST: api/LabWork/exec/5/5/true
+        // отметка о выполнении лаб. работы (параметр state)
+        [HttpPost]
+        [Route("api/LabWork/exec/{studentId}/{subjWorkId}/{state:bool}")]
+        public async Task<dynamic> PostExec([FromBody]string lab, string studentId, string subjWorkId, bool state=true)
+        {
+            //если state=false, запись удаляется из бд.
+            return null;
+        }
+
 
         // POST: api/LabWork
-        public void Post([FromBody]string value)
+        // добавление лабораторной работы
+        public async Task<dynamic> Post([FromBody]LabWork lab)
         {
+            return null;
         }
+
 
         // PUT: api/LabWork/5
-        public void Put(int id, [FromBody]string value)
+        // изменение лабораторной работы
+        public async Task<dynamic> Put(string id, [FromBody]LabWork lab)
         {
+            return null;
         }
 
-        // DELETE: api/LabWork/5
-        public void Delete(int id)
+
+        // DELETE: api/LabWork/plan/5
+        // удаление лабораторной работы из плана
+        [HttpDelete]
+        [Route("api/LabWork/plan/{id}")]
+        public async Task<dynamic> DeletePlan(string id)
         {
+            return null;
+        }
+
+
+        // DELETE: api/LabWork/5
+        // удаление лабораторной работы
+        public async Task<dynamic> Delete(string id)
+        {
+            return null;
         }
     }
 }
